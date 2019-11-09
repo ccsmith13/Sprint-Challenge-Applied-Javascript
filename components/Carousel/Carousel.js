fc3955f6f7b2b5eed80ec477af0d362b2fbe4eb8
Christine Smith <christinesm@google.com>
2019-11-09 11:37:02 -0800

    finished carousel but it refuses to show up on Chrome

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -9,11 +9,90 @@
 
 /* HTML:
   <div class="carousel">
-    <div class="left-button"> < </div>
+    <div class="left-button"> 
+    < 
+    </div>
     <img src="./assets/carousel/mountains.jpeg" />
     <img src="./assets/carousel/computer.jpeg" />
     <img src="./assets/carousel/trees.jpeg" />
     <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
+    <div class="right-button"> 
+    > 
+    </div>
   </div>
 */
+let imgList = [
+  "./assets/carousel/mountains.jpeg", 
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+];
+
+let currentImg = 0;
+
+function carouselCreator() {
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = "<";
+  rightButton.textContent = ">";
+
+  img.src = imgList[currentImg];
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(img);
+  carousel.appendChild(rightButton);
+
+  leftButton.addEventListener('click', () => {
+    if (currentImg > 0){
+      img.classList.toggle('left-animation')
+      currentImg--
+      setTimeout(()=>{
+        img.classList.toggle('left-animation')
+        img.src = imgList[currentImg]
+      }, 500)
+    } 
+    
+    else {
+      img.classList.toggle('left-animation');
+      currentImg = imgList.length - 1;
+      setTimeout(()=>{
+        img.classList.toggle('left-animation');
+        img.src = imgList[currentImg];
+      }, 500)
+    }
+  });
+
+  rightButton.addEventListener('click',() => {
+    if (currentImg < imgList.length -1){
+      img.classList.toggle('right-animation');
+      currentImg++;
+      setTimeout(()=>{
+        img.classList.toggle('right-animation');
+        img.src = imgList[currentImg];
+      }, 500);
+    }
+    
+    else{
+      img.classList.toggle('right-animation');
+      currentImg = 0;
+      setTimeout(() => {
+        img.classList.toggle('right-animation');
+        img.src = imgList[currentImg];
+      }, 500)
+    }
+  });
+  
+  return carousel;
+};
+
+let  carouselImages = document.querySelector('.carousel-container .carousel img');
+let carouselParent = document.querySelector('.carousel-container');
+carouselParent.appendChild(carouselCreator());
+
